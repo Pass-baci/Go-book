8b6af42c8a0683e630b3908bc2880a1ca518a2fd
陈国诚 <778281938@qq.com>
2022-01-24 17:25:50 +0800

    使用go-kit来进行consul的服务注册与发现

---
+++ ch04-discovery/discover/kit_discovery_client.go
@@ -0,0 +1,81 @@
+package discover
+
+import (
+	"github.com/go-kit/kit/sd/consul"
+	"github.com/hashicorp/consul/api"
+	"log"
+	"strconv"
+)
+
+type KitDiscoverClient struct {
+	Host   string
+	Port   int
+	client consul.Client
+}
+
+// 初始化KitDiscoveryClient
+func NewKitDiscoveryClient(consulHost string, consulPort int) (DiscoveryClient, error) {
+	consulConfig := api.DefaultConfig()
+	consulConfig.Address = consulHost + ":" + strconv.Itoa(consulPort)
+	apiClient, err := api.NewClient(consulConfig)
+	if err != nil {
+		return nil, err
+	}
+	consulClient := consul.NewClient(apiClient)
+	return &KitDiscoverClient{
+		Host:   consulHost,
+		Port:   consulPort,
+		client: consulClient,
+	}, nil
+}
+
+// 服务注册接口
+func (consulClient *KitDiscoverClient) Register(serviceName, instanceId, healthCheckUrl string, instanceHost string, instancePort int, meta map[string]string, logger *log.Logger) bool {
+	// 构建服务实例元数据
+	serviceRegistration := &api.AgentServiceRegistration{
+		ID:      instanceId,   //服务实例ID
+		Name:    serviceName,  // 服务名称
+		Address: instanceHost, // 服务地址
+		Port:    instancePort, // 服务端口
+		Meta:    meta,         // 服务元数据
+		Check: &api.AgentServiceCheck{ // 服务健康检查地址
+			DeregisterCriticalServiceAfter: "30s",
+			HTTP:                           "http://" + instanceHost + ":" + strconv.Itoa(instancePort) + healthCheckUrl,
+			Interval:                       "15s",
+		},
+	}
+	// 向consul中发起服务注册
+	if err := consulClient.client.Register(serviceRegistration); err != nil {
+		logger.Println("Register Service Error")
+		return false
+	}
+	logger.Println("DeRegister Service Success")
+	return true
+}
+
+// 服务注销接口
+func (consulClient *KitDiscoverClient) DeRegister(instanceId string, logger *log.Logger) bool {
+	serviceRegistration := &api.AgentServiceRegistration{
+		ID: instanceId, //服务实例ID
+	}
+	if err := consulClient.client.Deregister(serviceRegistration); err != nil {
+		logger.Println("DeRegister Service Error")
+		return false
+	}
+	logger.Println("DeRegister Service Success")
+	return true
+}
+
+// 发现服务实例接口
+func (consulClient *KitDiscoverClient) DiscoverServices(serviceName string, logger *log.Logger) []interface{} {
+	entries, _, err := consulClient.client.Service(serviceName, "", false, nil)
+	if err != nil {
+		logger.Println("Discover Services Error")
+		return nil
+	}
+	instances := make([]interface{}, len(entries))
+	for i := 0; i < len(entries); i++ {
+		instances[i] = entries[i].Service
+	}
+	return instances
+}
---
+++ ch04-discovery/go.mod
@@ -0,0 +1,11 @@
+module Go-/ch04-discovery
+
+go 1.16
+
+require (
+	github.com/go-kit/kit v0.12.0 // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/consul/api v1.12.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+)
